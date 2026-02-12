c56e7b4160f40be2ab6465849bc90731a8a6e06f
wanyeer703631302-cmyk <wanyeer703631302@gmail.com>
2026-02-12 22:13:22 +0800

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['picsum.photos', 'i.pravatar.cc']
   },
-  // 🚀 核心配置：跳过所有类型和格式检查，强制构建成功
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -15,9 +14,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        // 确保这里的后端地址是你最新的 Railway 地址
-        destination: 'https://yiz-guangzhou-production.up.railway.app/api/:path*',
+        // 👇 修改了这里：使用正则表达式排除掉 auth 路径
+        // 意思是：匹配 /api/ 中“不包含 auth”的所有路径
+        source: '/api/((?!auth).*)', 
+        destination: 'https://yiz-guangzhou-production.up.railway.app/api/:1*',
       },
     ]
   },
