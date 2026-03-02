7e9da52858f2d4d9d5cd5853989b067dfccb425d
wanyeer703631302-cmyk <wanyeer703631302@gmail.com>
2026-03-03 02:11:08 +0800

    添加 AuthProvider 和 Toaster 到应用根组件
    - 用 AuthProvider 包裹 App 组件以提供认证上下文
    - 添加 Toaster 组件用于显示通知消息
    - 修复 'useAuth must be used within an AuthProvider' 错误

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './contexts/AuthContext'
+import { Toaster } from 'sonner'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <AuthProvider>
+    <App />
+    <Toaster position="top-center" richColors />
+  </AuthProvider>
 )
